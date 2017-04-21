7f1acd63508ff5c587cea0bc46b400f930a4345d
Tyler Gaona <gaonat@duq.edu>
2017-04-21 10:26:56 -0400

    initial work on game lobby

--- client.js
+++ client.js
@@ -1,21 +1,55 @@
 var socket = io();
+var userid;
 
-// When the user clicks the join button, add their name to the lobby.
+// When the user clicks the join button, tell the server a new user has joined.
 var button = document.getElementById('join_button');
 button.addEventListener('click', function(event) {
     var input = document.getElementById('username_input');
     if (input.value) {
-	// Tell the server that someone has joined the lobby.
-	socket.emit('join lobby', input.value);
-	var lobby = document.getElementById('lobby');
-	var li = document.createElement('li');
-	li.innerHTML = input.value;
-	lobby.appendChild(li);	
-	// Remove the input and button.
-	document.body.removeChild(input);
-	document.body.removeChild(button);
+	userid = input.value;
+	socket.emit('new user', input.value);
     }
 });
+
+// When the server responds with a new user, add them to the lobby.
+socket.on('new user', function(username) {
+    var lobby = document.getElementById('lobby');
+    var li = document.createElement('li');
+    li.innerHTML = username;
+    lobby.appendChild(li);
+});
+
+socket.on('remove input', function() {
+    document.body.removeChild(document.getElementById('username_input'));
+    document.body.removeChild(document.getElementById('join_button'));
+});
+
+socket.on('delete user', function(name) {
+    var lobby = document.getElementById('lobby');
+    if (lobby.hasChildNodes()) {
+	for (var i = 0; i < lobby.childNodes.length; i++) {
+	    if (lobby.childNodes[i].innerHTML === name) {
+		lobby.removeChild(lobby.childNodes[i]);
+		return;
+	    }
+	}
+    }
+});
+
+socket.on('init lobby', function(data) {
+    var users = JSON.parse(data);
+    if (users && users.length != 0) {
+	var lobby = document.getElementById('lobby');
+	users.forEach(function(user) {
+	    var li = document.createElement('li');
+	    li.innerHTML = user.name;
+	    lobby.appendChild(li);
+	});
+    }
+});    
+	    
+	    
+    
 
 
 	
--- server.js
+++ server.js
@@ -16,9 +16,44 @@ var server = http.createServer(function(request, response) {
     }
 });
 server.listen(2718);
-console.log("Listening on 2718...");
 
 var io = require("socket.io")(server);
+
+var users = [];
+
+var newUser = function(id, name) {
+    return {
+	'id': id,
+	'name': name
+    };
+};
+
+var getUser = function(id) {
+    return users.find(function(user) {
+	return user.id === id;
+    });
+}
+
 io.on('connection', function(socket) {
     console.log("a user connected...");
+
+    socket.emit('init lobby', JSON.stringify(users));
+
+    socket.on('new user', function(username) {
+	console.log("a new user " + username + " joined...");
+	users.push(newUser(socket.id, username));
+	io.emit('new user', username);
+	socket.emit('remove input');
+    });
+
+    socket.on('disconnect', function(reason) {
+	var user = getUser(socket.id);
+	if (user) {
+	    console.log(user.name + ' is disconnecting...');
+	    users.splice(users.indexOf(user), 1);
+	    io.emit('delete user', user.name);
+	}
+    });
 });
+
+
